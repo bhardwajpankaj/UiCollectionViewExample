f64efc8d8d828c50b45596f84143e9ea0138878d
Pankaj <pbhardwaj@souq.com>
2016-07-21 12:36:58 +0530

    Initial Commit

---
+++ Collection_learning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Collection_learning
+//
+//  Created by Pankaj on 21/07/16.
+//  Copyright © 2016 Pankaj. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
